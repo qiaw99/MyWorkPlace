ceb12ee11fe11b954c928b25793702c48ac05399
Nazar Sopiha <43066074+sopa40@users.noreply.github.com>
2019-06-13 13:13:53 +0200

    Lock and shared memory combined. w/p comments

--- client.c
+++ client.c
@@ -3,35 +3,40 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
-#define FileName "test.txt"
+#define PROJECT_ID 65
 
-int main() {
-	struct flock lock;
-	lock.l_type = F_WRLCK;   
-	lock.l_whence = SEEK_SET; 
-	lock.l_start = 0;         
-	lock.l_len = 0;           
-	lock.l_pid = getpid();   
-
-	int fd; 
-	if ((fd = open(FileName, O_RDONLY)) < 0)  
-		fprintf(stderr,"Can't be opened!\n");
-
-	fcntl(fd, F_GETLK, &lock);
-	if (lock.l_type != F_UNLCK)
-		fprintf(stderr,"Write locked!\n");
-
-	lock.l_type = F_RDLCK; 
-	if (fcntl(fd, F_SETLK, &lock) < 0)
-		fprintf(stderr,"Can't get a read-only lock!\n");
-
-	int c; 
-	while (read(fd, &c, 1) > 0)    
-		write(STDOUT_FILENO, &c, 1); 
-	lock.l_type = F_UNLCK;
-	if (fcntl(fd, F_SETLK, &lock) < 0)
-		fprintf(stderr,"Can't be unlocked!\n");
-	close(fd);
-	return 0;
+int main()
+{
+    struct flock lock;
+    int shmid;
+    int fd;
+    key_t key;
+    char *str;
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+    lock.l_pid = getpid();
+    key = ftok("shm_file",PROJECT_ID);
+    shmid = shmget(key,1024,0666|IPC_CREAT);
+    str = (char*) shmat(shmid,(void*)0,0);
+    printf("Data read from memory: %s\n",str);
+    if ((fd = open("shm_file", O_RDONLY)) < 0)
+        fprintf(stderr,"Can't be opened!\n");
+    fcntl(fd, F_GETLK, &lock);
+    if (lock.l_type != F_UNLCK)
+        fprintf(stderr,"Write locked!\n");
+    lock.l_type = F_RDLCK;
+    if (fcntl(fd, F_SETLK, &lock) < 0)
+        fprintf(stderr,"Can't get a read-only lock!\n");
+    lock.l_type = F_UNLCK;
+    if (fcntl(fd, F_SETLK, &lock) < 0)
+        fprintf(stderr,"Can't be unlocked!\n");
+    close(fd);
+    shmdt(str);
+    shmctl(shmid,IPC_RMID,NULL);
+    return 0;
 }
