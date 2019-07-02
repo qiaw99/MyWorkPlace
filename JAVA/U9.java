441d13d583e8951e97026e7590c90bffd95f1f88
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-07-02 18:06:52 +0200

    Create U9.java

---
+++ JAVA/U9.java
@@ -0,0 +1,212 @@
+/*
+Übungsgruppe: Qianli und Nazar
+*/
+
+class EmptyQueueException extends Exception{
+	private String retCd;
+    private String msgDes;
+    public EmptyQueueException(){
+        super();
+    }
+    public EmptyQueueException(String message){
+        super(message);
+        msgDes=message;
+    }
+    public EmptyQueueException(String retCd, String msgDes){
+        super();
+        this.retCd=retCd;
+        this.msgDes=msgDes;
+    }
+    public String getRetCd(){
+        return this.retCd;
+    }
+    public String getMsgDes(){
+        return this.msgDes;
+    }
+}
+class FullQueueException extends Exception{
+	private String retCd;
+    private String msgDes;
+    public FullQueueException(){
+        super();
+    }
+    public FullQueueException(String message){
+        super(message);
+        msgDes=message;
+    }
+    public FullQueueException(String retCd, String msgDes){
+        super();
+        this.retCd=retCd;
+        this.msgDes=msgDes;
+    }
+    public String getRetCd(){
+        return this.retCd;
+    }
+    public String getMsgDes(){
+        return this.msgDes;
+    }
+}
+interface Queue<E>{
+	public void enqueue(E element)throws FullQueueException;
+	public E dequeue() throws EmptyQueueException;
+	public E first() throws EmptyQueueException;
+	public boolean empty();
+	public String toString();
+}
+interface Iterable<E>{
+    Iterator<E> iterator();
+}
+
+interface Iterator<T>{
+    public boolean hasNext();
+    public T next();
+}
+class simpleQueue<E> implements Queue<E>{
+	private int head;
+	private int tail;
+	private int current;
+	private E[] queue;
+	public simpleQueue(){
+		this((E[]) new Object[10]);		/*assert the length of queue is 10*/
+	}
+	public simpleQueue(E[] queue){
+		head=tail=0;
+		this.queue=queue;
+	}
+	public boolean empty(){
+		return head==tail;
+	}
+	public boolean full(){
+		return ((tail==queue.length-1)&&(head==0))||(head==(tail+1));
+	}
+	public void enqueue(E elem) throws FullQueueException{
+		if(!full()){
+			queue[tail]=elem;
+			if(tail==(queue.length-1)){
+				tail=0;
+			}else{
+				tail++;
+			}
+		}else{
+			throw new FullQueueException("There is no space in the queue to enqueue!");
+		}
+	}
+	public E dequeue() throws EmptyQueueException{
+		if(!empty()){
+			E elem=queue[head];
+			if(head==(queue.length-1)){
+				head=0;
+			}else{
+				head++;
+			}
+			return elem;
+		}else{
+			throw new EmptyQueueException("There is no element in the queue to dequeue!");
+		}
+	}
+	public E first() throws EmptyQueueException{
+		if(full()){
+			throw new EmptyQueueException("There is no element in the queue!");
+		}else{
+			E elem = queue[head];
+			return elem;
+		}
+	}
+	public String toString(){
+		current=this.head;
+		String s="";
+		if(head>tail){
+			while(current<queue.length-1){
+				s+=queue[current];
+				current++;
+			}
+			s+=queue[current];
+			current=0;
+			while(current<=tail){
+				s+=queue[current];
+			}
+			return s;
+		}else{
+			while(current<=tail){
+				s=s+queue[current]+" ";
+				current++;
+			}
+			return s;
+		}	
+	}
+	class QueueIterator<T> implements Iterator<E>{	/*Innere Klasse*/
+        int current;
+        public QueueIterator(){
+            current=head;
+        }
+        public boolean hasNext(){
+            return queue[current]!=null;
+        }
+        public E next(){
+        	E temp = queue[current+1];
+        	if(current<queue.length-1)
+        		current++;
+        	else{
+        		current=0;
+        	}
+        	return temp;
+        	
+        }
+	}
+    public Iterator<E> iterator(){		/*Iterator-Schnittstelle implementieren*/
+        return new QueueIterator<E>();
+    }
+}
+class TestArrayQueue {
+	public static void test() throws Exception{
+		simpleQueue <String> str = new simpleQueue <String>();
+		Iterator i=str.iterator();
+		System.out.println("Enqueue with element \"1\"");
+        str.enqueue("1");
+        System.out.println("Queue now: "+str.toString());
+
+        System.out.println("Enqueue with element \"2\"");
+        str.enqueue("2");
+        System.out.println("Queue now: "+str.toString());
+
+        System.out.println("Enqueue with element \"3\"");
+        str.enqueue("3");
+        System.out.println("Queue now: "+str.toString());
+
+        System.out.println("Enqueue with element \"4\"");
+        str.enqueue("4");
+        System.out.println("Queue now: "+str.toString());
+
+        System.out.println("Enqueue with element \"5\"");
+        str.enqueue("5");
+        System.out.println("Queue now: "+str.toString());
+
+        System.out.println("Enqueue with element \"6\"");
+        str.enqueue("6");
+        System.out.println("Queue now: "+str.toString());
+        System.out.println("See the first element");
+        System.out.println(str.first());
+        System.out.println("What about dequeue?");
+        System.out.println(str.dequeue());
+        System.out.println("The queue now?");
+        System.out.println(str.toString());
+        System.out.println("What about dequeue?");
+        System.out.println(str.dequeue());
+        System.out.println("The queue now?");
+        System.out.println(str.toString());
+        System.out.println("The result using Iterator:");        
+        for(;i.hasNext();){
+            System.out.println("Has next element?");
+            System.out.println(i.hasNext());
+        	System.out.println("The element is: "+i.next());
+        }
+	}
+}
+public class U9{
+	public static void main(String args[]) throws Exception{
+		TestArrayQueue.test();
+	}
+}
+
+
+
